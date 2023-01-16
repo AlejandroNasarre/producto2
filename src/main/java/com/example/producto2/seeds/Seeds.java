7116b283366ce56b05625e71dfd756397d4ee4c0
pedro.segarra <psegarrac@uoc.edu>
2023-01-16 16:09:11 +0100

    modificacion menu

--- src/main/java/com/example/producto2/seeds/Seeds.java
+++ src/main/java/com/example/producto2/seeds/Seeds.java
@@ -9,6 +9,14 @@ public class Seeds {
     public Seeds(MenuRepository menuRepository) {this.menuRepository = menuRepository; }
 
     public void generateSeeds(){
+
         Menu menuLibanes = new Menu("Libanes", 30.5);
+        Menu menuVeggie = new Menu("Veggie", 20);
+        Menu menuBurguer = new Menu("Libanes", 25);
+
+        menuRepository.save(menuLibanes);
+        menuRepository.save(menuVeggie);
+        menuRepository.save(menuBurguer);
     }
+
 }
